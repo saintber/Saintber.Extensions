3a934b98d0291585fa7e6c0cddcad3ec3b1febb9
saintber <saintber@gmail.com>
2025-03-06 22:27:46 +0800

    新增交易擴充函式

---
+++ src/Saintber.Extensions/Transactions/TransactionExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System.Transactions;
+
+namespace Saintber.Extensions.Transactions
+{
+    public static class TransactionExtensions
+    {
+        /// <summary>
+        /// 啟動交易以執行函數。
+        /// </summary>
+        /// <typeparam name="T">函數回傳型別。</typeparam>
+        /// <param name="func">執行函數。</param>
+        /// <param name="timeout">交易逾時時間。</param>
+        /// <returns>非同步作業。</returns>
+        public static async Task<T> TransactionAsync<T>(this TimeSpan timeout, Func<Task<T>> func)
+        {
+            using (TransactionScope ts = new TransactionScope(TransactionScopeOption.Required,
+                new TransactionOptions { IsolationLevel = IsolationLevel.RepeatableRead, Timeout = timeout }
+                , TransactionScopeAsyncFlowOption.Enabled))
+            {
+                var response = await func();
+                ts.Complete();
+                return response;
+            }
+        }
+
+        /// <summary>
+        /// 啟動交易以執行函數。
+        /// </summary>
+        /// <param name="func">執行函數。</param>
+        /// <param name="timeout">交易逾時時間。</param>
+        /// <returns>非同步作業。</returns>
+        public static async Task TransactionAsync(this TimeSpan timeout, Func<Task> func)
+        {
+            using (TransactionScope ts = new TransactionScope(TransactionScopeOption.Required,
+                new TransactionOptions { IsolationLevel = IsolationLevel.RepeatableRead, Timeout = timeout }
+                , TransactionScopeAsyncFlowOption.Enabled))
+            {
+                await func();
+                ts.Complete();
+            }
+        }
+    }
+}
